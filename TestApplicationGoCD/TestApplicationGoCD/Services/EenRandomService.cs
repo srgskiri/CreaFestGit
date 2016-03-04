8e6bf2b02d1b2e774a7a5f18b400b0c443094e3e
srgskiri <go_thrall@msn.com>
2016-03-04 14:56:39 +0100

    UnitTests

---
+++ TestApplicationGoCD/TestApplicationGoCD/Services/EenRandomService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApplicationGoCD.Services
+{
+    public class EenRandomService
+    {
+        public EenRandomService()
+        {
+               
+        }
+
+        public int EenFunctie()
+        {
+            return 4;
+        }
+    }
+}
---
+++ TestApplicationGoCD/TestApplicationGoCDTests/Services/EenRandomServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplicationGoCD.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestApplicationGoCD.Services.Tests
+{
+    [TestClass()]
+    public class EenRandomServiceTests
+    {
+        [TestMethod()]
+        public void EenFunctieTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
